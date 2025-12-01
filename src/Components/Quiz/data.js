4e9fcb9f79af000805dc29693cfc1d4dfb1e0695
TorraCat16 <mariana.papikiani.1@iliauni.edu.ge>
2025-12-01 20:35:32 +0400

    initial

---
+++ src/Components/Quiz/data.js
@@ -0,0 +1,82 @@
+export const data = [
+  {
+    "question": "Which device is required for the Internet connection?",
+    "option1": "Modem",
+    "option2": "Router",
+    "option3": "LAN Cable",
+    "option4": "Pen Drive",
+    "ans": 1
+  },
+  {
+    "question": "Which programming language is primarily used for web development?",
+    "option1": "Python",
+    "option2": "HTML",
+    "option3": "C++",
+    "option4": "Java",
+    "ans": 2
+  },
+  {
+    "question": "Which of the following is an operating system?",
+    "option1": "Microsoft Word",
+    "option2": "Windows",
+    "option3": "Google Chrome",
+    "option4": "Adobe Photoshop",
+    "ans": 2
+  },
+  {
+    "question": "What does CPU stand for?",
+    "option1": "Central Processing Unit",
+    "option2": "Computer Primary Unit",
+    "option3": "Central Peripheral Unit",
+    "option4": "Control Processing Utility",
+    "ans": 1
+  },
+  {
+    "question": "Which of the following is a type of non-volatile memory?",
+    "option1": "RAM",
+    "option2": "ROM",
+    "option3": "Cache",
+    "option4": "Register",
+    "ans": 2
+  },
+  {
+    "question": "Which protocol is used to send emails?",
+    "option1": "HTTP",
+    "option2": "FTP",
+    "option3": "SMTP",
+    "option4": "DNS",
+    "ans": 3
+  },
+  {
+    "question": "What is the main function of a firewall?",
+    "option1": "Increase internet speed",
+    "option2": "Protect network from unauthorized access",
+    "option3": "Store backup data",
+    "option4": "Monitor CPU usage",
+    "ans": 2
+  },
+  {
+    "question": "Which language is used for Android app development?",
+    "option1": "Java",
+    "option2": "Python",
+    "option3": "C#",
+    "option4": "Ruby",
+    "ans": 1
+  },
+  {
+    "question": "What does HTTP stand for?",
+    "option1": "HyperText Transfer Protocol",
+    "option2": "HyperText Transmission Program",
+    "option3": "High Transfer Text Protocol",
+    "option4": "Hyperlink Text Transfer Process",
+    "ans": 1
+  },
+  {
+    "question": "Which of the following is a type of computer network?",
+    "option1": "WAN",
+    "option2": "LAN",
+    "option3": "MAN",
+    "option4": "All of the above",
+    "ans": 4
+  }
+]
